fb8376c8514abbb248b29315fb1eb67671167df8
Cameron Powers <cpowers@sonifi.com>
2023-02-10 10:42:17 -0700

    Added auth sub applicaition

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,23 +4,23 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up application
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initalPath }) => {
     const history = defaultHistory || createMemoryHistory({
-        initialEntries: [initialPath]
+        initialEntries: [initalPath]
     });
     if (onNavigate){
         history.listen(onNavigate);
     }
 
     ReactDOM.render(
-        <App history={history}/>,
+        <App onSignIn={onSignIn} history={history}/>,
         el
     );
 
     return {
         onParentNavigate({ pathname: nextPathname}) {
             const { pathname } = history.location;
-
+            console.log(nextPathname);
             if (pathname !== nextPathname){
                 history.push(nextPathname);
             }
@@ -30,7 +30,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If development and in isolation
 if(process.env.NODE_ENV === 'development'){
-    const devRoot = document.querySelector('#_marketing-dev-root')
+    const devRoot = document.querySelector('#_auth-dev-root')
 
     if(devRoot){
         mount(devRoot, { defaultHistory: createBrowserHistory()});
